79d3b30676d560f1cf12a72ce75739669838839e
aimeiyijia <1456723200@qq.com>
2023-08-04 23:57:30 +0800

    init

---
+++ libs/navigation/navigation.js
@@ -0,0 +1,330 @@
+/**
+ * 页面展示列表数据
+ */
+export default {
+  data: [{
+      title: '爱魅首页',
+      backgroundColor: 'tn-cool-bg-color-1',
+      list: [{
+          icon: 'code',
+          title: '关于我们',
+          url: '/homePages/about',
+          author: '爱魅小凡'
+        },{
+          icon: 'code',
+          title: '全局搜索',
+          url: '/homePages/search',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '今日热榜',
+          url: '/homePages/hot',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '前端业务',
+          url: '/homePages/profession',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '加载效果',
+          url: '/homePages/loading',
+          author: '爱魅小凡'
+        }
+      ]
+    },
+    {
+      title: '酷炫圈子',
+      backgroundColor: 'tn-cool-bg-color-1',
+      list: [{
+          icon: 'code',
+          title: '博主_Me',
+          url: '/circlePages/blogger',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '博主_Ta',
+          url: '/circlePages/blogger_other',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '编辑发布',
+          url: '/circlePages/edit',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '广告页',
+          url: '/circlePages/advertise',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '资讯详情',
+          url: '/circlePages/news',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '名片王者',
+          url: '/circlePages/king',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '智能名片',
+          url: '/circlePages/business',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '精选圈子',
+          url: '/circlePages/group',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '积分排行',
+          url: '/circlePages/ranking',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '圈子详情',
+          url: '/circlePages/details',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '预约接龙',
+          url: '/circlePages/reserve',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '活动创建',
+          url: '/circlePages/create',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '打造圈子',
+          url: '/circlePages/build',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '一起群聊',
+          url: '/circlePages/chat',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '对话聊天',
+          url: '/circlePages/chatting',
+          author: '爱魅小凡'
+        }
+      ]
+    },
+    {
+      title: '活动广场',
+      backgroundColor: 'tn-cool-bg-color-1',
+      list: [{
+          icon: 'code',
+          title: '地图打卡',
+          url: '/activityPages/map',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '快速答题',
+          url: '/activityPages/topic',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '课程学习',
+          url: '/activityPages/study',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '开源项目',
+          url: '/activityPages/project',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '活动星球',
+          url: '/activityPages/planet',
+          author: '爱魅小凡'
+        }
+      ]
+    },
+    {
+      title: '商品优选',
+      backgroundColor: 'tn-cool-bg-color-1',
+      list: [{
+          icon: 'code',
+          title: '优质商家',
+          url: '/preferredPages/shop',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '商品详情',
+          url: '/preferredPages/product',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '历史订单',
+          url: '/preferredPages/order',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '商品分类',
+          url: '/preferredPages/classify',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '商家相册',
+          url: '/preferredPages/photo',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '品牌官网',
+          url: '/preferredPages/website',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '积分兑换',
+          url: '/preferredPages/redeem',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '免单活动',
+          url: '/preferredPages/award',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '免单获取',
+          url: '/preferredPages/awardget',
+          author: '爱魅小凡'
+        }
+      ]
+    },
+    {
+      title: '关于我的',
+      backgroundColor: 'tn-cool-bg-color-1',
+      list: [{
+          icon: 'code',
+          title: '使用协议',
+          url: '/minePages/protocol',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '授权登录',
+          url: '/minePages/login',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '消息通知',
+          url: '/minePages/message',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '全局设置',
+          url: '/minePages/set',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '立即体验',
+          url: '/minePages/start',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '感谢名单',
+          url: '/minePages/thanks',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '版本更新',
+          url: '/minePages/version',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '帮助中心',
+          url: '/minePages/help',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '头像上传',
+          url: '/minePages/avatar',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '积分明细',
+          url: '/minePages/integral',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '积分签到',
+          url: '/minePages/signed',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '好物收藏',
+          url: '/minePages/collect',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '账号安全',
+          url: '/minePages/safety',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '赞赏支持',
+          url: '/minePages/reward',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '缺省页',
+          url: '/minePages/default',
+          author: '爱魅小凡'
+        },
+        {
+          icon: 'code',
+          title: '富文本',
+          url: '/minePages/content',
+          author: '爱魅小凡'
+        }
+      ]
+    }
+  ]
+}
